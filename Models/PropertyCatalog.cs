5f8babd1f0b072ba2ca71e639926f340da095cb8
neemilis <emilisgum@gmail.com>
2016-12-03 03:13:44 +0100

    Property
    Added appartments in each property
    Adding ID, price and people limit to each apartment (room)
    View and ModelView need some changes. I'll do it on this Saturday!
    (PropertiesPage)

--- Models/PropertyCatalog.cs
+++ Models/PropertyCatalog.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using School.OnlineBookingSystem.Common;
 
 namespace School.OnlineBookingSystem.Models
@@ -18,6 +19,41 @@ namespace School.OnlineBookingSystem.Models
         {
             FilePath = "properties_data.json";
             LoadCollection();
+            foreach (var prop in Collection)
+            {
+                Dictionary<string, Apartment> apartments = new Dictionary<string, Apartment>();
+                // Creating Apartments
+
+                for (int i = 0; i < prop.NumberOfSmallRooms; i++)
+                {
+                    int maxPeople = 5;
+                    prop.Id = CreateID(prop.Type, prop.Code, "SR", i); // SR - Small Room apartment type 
+                    apartments.Add(prop.Id, new Apartment(prop.PriceOfSmallRoom, maxPeople));
+                }
+                for (int i = 0; i < prop.NumberOfBigRooms; i++)
+                {
+                    int maxPeople = 5;
+                    prop.Id = CreateID(prop.Type, prop.Code, "BR", i);
+                    apartments.Add(prop.Id, new Apartment(prop.PriceOfBigRoom, maxPeople));
+                }
+            }
+        }
+        private string CreateID(string _propertyType, string _code, string _apartmentType, int _nr)
+        {
+            string _nrString;
+            if (_nr < 10)
+            {
+                _nrString = "00" + _nr;
+            }
+            else if (_nr < 100 && _nr >= 10)
+            {
+                _nrString = "0" + _nr;
+            }
+            else _nrString = _nr.ToString();
+
+            string _Id = $"{_propertyType}{_code}_{_apartmentType}{_nrString}";
+            Debug.WriteLine(_Id);
+            return _Id;
         }
     }
 }
