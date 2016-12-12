0b8ece68e447bec6ce4489be06e7b3c219529600
neemilis <emilisgum@gmail.com>
2016-12-12 12:54:34 +0100

    BookingCat

--- Models/BookingCatalog.cs
+++ Models/BookingCatalog.cs
@@ -8,18 +8,12 @@ namespace School.OnlineBookingSystem.Models
 {
     public sealed class BookingCatalog : Catalog<Booking>
     {
-        private Property prop1 { get; set; }
-        private Property prop2 { get; set; }
-        private Property prop3 { get; set; }
-
-        // Load all bookings
         public BookingCatalog()
         {
             FilePath = "booking_data.json";
             LoadCollection();
         }
 
-        // When we adding a new bookingInfo to propertyName.json
         public BookingCatalog(Booking bookingInfo, Property property)
         {
             FilePath = property.Name + ".json";
@@ -27,23 +21,6 @@ namespace School.OnlineBookingSystem.Models
             Collection = new ObservableCollection<Booking>();
             Collection.Add(bookingInfo);
             SaveCollection();
-        }
-
-        private string CreateID(string _apartmentTypeInitial, int _nr)
-        {
-            string _nrString;
-            if (_nr < 10)
-            {
-                _nrString = "00" + _nr;
-            }
-            else if (_nr < 100 && _nr >= 10)
-            {
-                _nrString = "0" + _nr;
-            }
-            else _nrString = _nr.ToString();
-
-            string _Id = $"{_apartmentTypeInitial}{_nrString}";
-            return _Id;
         }
     }
 }
